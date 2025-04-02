d91f7df329dc0b03d3b6d1cb469f840303ab94cd
NoReply Amazon <no-reply@amazon.com>
2025-04-02 12:37:57 +0000

    [Commit] Iteration 14 #errors = 3 <= 3.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/WcfServiceLibrary.WindowsServiceHost/Service1.cs][CS0246][13,9] The type or namespace name 'ServiceHost' could not be found (are you missing a using directive or an assembly reference?)``.

--- WcfServiceLibrary.WindowsServiceHost/Service1.cs
+++ WcfServiceLibrary.WindowsServiceHost/Service1.cs
@@ -2,7 +2,7 @@ using System.ServiceProcess;
 using System.Diagnostics;
 using System;
 using CoreWCF;
-using System.ServiceProcess;
+using CoreWCF.Configuration;
 
 
 namespace WcfServiceLibrary.WindowsServiceHost
@@ -10,12 +10,12 @@ namespace WcfServiceLibrary.WindowsServiceHost
     public partial class Service1 : ServiceBase
     {
 
-        ServiceHost serviceHost; 
+        CoreWCF.ServiceHost serviceHost;
 
         public Service1()
         {
             InitializeComponent();
-            serviceHost = new ServiceHost(typeof(WcfServiceLibrary.Service1));
+            serviceHost = new CoreWCF.ServiceHost(typeof(WcfServiceLibrary.Service1));
         }
 
         internal void StartInDebug()
