11ac8178a1a1e1e269fb8084c6f3837a7e5a9123
NoReply Amazon <no-reply@amazon.com>
2025-04-02 12:31:15 +0000

    [Commit] Iteration 7 #errors = 18 <= 16.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/WcfServiceLibrary.SampleHost/Program.cs][CS0246][17,13] The type or namespace name 'ChannelFactory<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- WcfServiceLibrary.SampleHost/Program.cs
+++ WcfServiceLibrary.SampleHost/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CoreWCF;
+using System.ServiceModel;
 
 
 namespace WcfServiceLibrary.SampleHost
@@ -12,8 +13,8 @@ namespace WcfServiceLibrary.SampleHost
     {
         static void Main(string[] args)
         {
-            CoreWCF.BasicHttpBinding wSHttpBinding = new CoreWCF.BasicHttpBinding();
-            CoreWCF.EndpointAddress endpointAddress = new CoreWCF.EndpointAddress(@"http://localhost:8743/testservice/WcfServiceLibrary");
+            BasicHttpBinding wSHttpBinding = new BasicHttpBinding();
+            EndpointAddress endpointAddress = new EndpointAddress(@"http://localhost:8743/testservice/WcfServiceLibrary");
             ChannelFactory<IService1> channelFactory = new ChannelFactory<IService1>(wSHttpBinding, endpointAddress);
             IService1 proxyObject = channelFactory.CreateChannel();
 
